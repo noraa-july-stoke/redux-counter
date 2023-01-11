d5522d858750a8d0a69198ae7f4fce7c1ea06938
Noraa Stoke <noraa.july.stoke@gmail.com>
2023-01-11 11:48:13 -0500

    ok

--- src/components/widgets/time/Time.jsx
+++ src/components/widgets/time/Time.jsx
@@ -3,6 +3,8 @@ import { useSelector, useDispatch } from 'react-redux';
 import './Time.scss';
 const Time = () => {
 
+    const time = useSelector(store => store.time)
+
     return (
         <div className="time-container">
             <h2>Time Display: </h2>
